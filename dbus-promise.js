a288fa205be21c8326c5d27cb01fe12898c9421d
Tim Perry <pimterry@gmail.com>
2017-05-05 20:05:46 +0200

    WIP use the raw from-coffeescript dbus code

--- dbus-promise.js
+++ dbus-promise.js
@@ -1,25 +1,41 @@
-const Promise = require('bluebird');
-const DBus = require('dbus');
-const Bus = require('dbus/lib/bus');
-const Interface = require('dbus/lib/interface');
+var Bus, DBus, Interface, Promise, oldInit,
+  slice = [].slice,
+  hasProp = {}.hasOwnProperty;
 
-Promise.promisifyAll(Bus.prototype)
-Promise.promisifyAll(Interface.prototype)
+Promise = require('bluebird');
+DBus = require('dbus');
+Bus = require('dbus/lib/bus');
+Interface = require('dbus/lib/interface');
 
-const oldInit = Interface.init;
+Promise.promisifyAll(Bus.prototype);
+Promise.promisifyAll(Interface.prototype);
 
-Interface.init = function (...args) {
-	oldInit.apply(this, args)
+oldInit = Interface.prototype.init;
 
-    for (method of this.object.method) {
-        this[method + 'Async'] = (...methodArgs) =>
-            new Promise((resolve, reject) => {
-                this[method].timeout = 5000;
-                this[method].finish = resolve;
-                this[method].error = reject;
-                this[method](...methodArgs);
-            });
-    }
-}
+Interface.prototype.init = function() {
+  var args, method, ref, results;
+  args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+  oldInit.apply(this, args);
+  ref = this.object.method;
+  results = [];
+  for (method in ref) {
+    if (!hasProp.call(ref, method)) continue;
+    results.push(this[method + 'Async'] = (function(method) {
+      return function() {
+        var args;
+        args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+        return new Promise((function(_this) {
+          return function(resolve, reject) {
+            _this[method].timeout = 5000;
+            _this[method].finish = resolve;
+            _this[method].error = reject;
+            return _this[method].apply(_this, args);
+          };
+        })(this));
+      };
+    })(method));
+  }
+  return results;
+};
 
 module.exports = DBus;
